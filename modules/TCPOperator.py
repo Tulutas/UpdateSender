a11093816931b3542ed3152c5537ae5ed6b2ea2f
Tulutas <kom.tolga.ulutas@gmail.com>
2023-06-03 17:38:25 +0300

    First commit

--- modules/TCPOperator.py
+++ modules/TCPOperator.py
@@ -4,18 +4,24 @@ from queue import Queue
 
 class TCPClass():
     """This class will be used a creating TCP Server and Client Applications"""
-    def init__(self,hostname,listen_port):
+    def __init__(self,hostname,listen_port):
         self.hostname=hostname
         self.listen_port=listen_port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        self.output_que=Queue()
 
     def open_tcp_server(self):
         # Bind the socket to the port
         self.server_address = (self.hostname, self.listen_port)
+        self.output_que=Queue()
         print ('starting up on port %s',self.server_address)
         self.sock.bind(self.server_address)
-    def start_echo_listening(self,output_que):
+    
+    def data_received(self,data):
+        self.output_que.append(data)
+        
+
+    def start_echo_listening(self):
         """ This function listens a socket and returns echo messsage"""
         self.sock.listen(1)
         while True:
@@ -26,13 +32,13 @@ class TCPClass():
                     data = connection.recv(16)
                     print('received "%s"' , data)
                     if data:
-                        output_que.append(data)
+                        self.data_received(data)
                         connection.sendall(data)
                     else:
                         break
             finally:
                 connection.close()
-    def start_listening(self,output_que):
+    def start_listening(self):
         """ This function listens a socket """
         self.sock.listen(1)
         while True:
@@ -43,7 +49,7 @@ class TCPClass():
                     data = connection.recv(16)
                     print('received "%s"' , data)
                     if data:
-                        output_que.append(data)
+                        self.data_received(data)
                     else:
                         break
             finally:
@@ -55,3 +61,7 @@ class TCPClass():
         self.sock.sendall(b"Hello, world")
         data = self.sock.recv(1024)
         print(str(data))
+
+newobject=TCPClass("127.0.0.1",30001)
+newobject.open_tcp_server()
+newobject.start_echo_listening()
